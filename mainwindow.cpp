e5aa8b536d9f0101185e33c7619f8c73e9a98fa7
Juan Jose Alvarez Rodriguez <63651794+Juan-Alvarez999@users.noreply.github.com>
2020-05-18 08:53:37 -0500

    Add files via upload
    Codigo en C++ de la ventana principal en la interfaz.

---
+++ mainwindow.cpp
@@ -0,0 +1,49 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("Menú");
+    QTimer *timer=new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(showTime()));
+    timer->start();
+
+    QDateTime dateTime=QDateTime::currentDateTime();
+    QString datetimetex=dateTime.toString("dd/MM/yyyy");
+    ui->fecha->setText(datetimetex);
+
+}
+
+
+void MainWindow::showTime()
+{
+    QTime time=QTime::currentTime();
+    QString time_text=time.toString("hh : mm : ss");
+    ui->hora->setText(time_text);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::on_datos_clicked()
+{
+    hide();
+    carnet = new Carnet(this);
+    carnet->show();
+
+}
+
+
+
+void MainWindow::on_salir_clicked()
+{
+    close();
+}
